d59e7f3ad1dcbd65a66e8d49944a01511ecd0df1
Adwait <adwait.kul.2018@gmail.com>
2022-12-18 13:00:21 -0800

    Add next_question method

--- quiz_brain.py
+++ quiz_brain.py
@@ -3,3 +3,6 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = question_list
         
+    def next_question(self):
+        currentItem = self.question_list[self.question_number]
+        input(f"Q. {self.question_number}: {currentItem.text} (True/False): ")
