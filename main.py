2e6f6131ef584e69f4b21df22680db3fa39000b3
Adwait <adwait.kul.2018@gmail.com>
2022-12-18 18:09:46 -0800

    Add API to randomize questions every time

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    tmp = Question(question["text"], question["answer"])
+    tmp = Question(question["question"], question["correct_answer"])
     question_bank.append(tmp)
     
 quiz = QuizBrain(question_bank)
